a4fbf88710dc673f81a585bd3cf908ab9af23712
i.mynah@gmail.com <i.mynah@gmail.com@fc34eba4-b4c5-fea7-dd0e-98b2151f68ae>
2012-01-09 14:52:24 +0000

    Matrix testcases

--- trunk/math/Matrix3fTest.java
+++ trunk/math/Matrix3fTest.java
@@ -44,7 +44,32 @@ public class Matrix3fTest
 		assertEquals(8.0f*scale.y, m3f.m21, 0.000001);
 		assertEquals(3.0f*scale.z, m3f.m02, 0.000001);
 		assertEquals(6.0f*scale.z, m3f.m12, 0.000001);
-		assertEquals(9.0f*scale.z, m3f.m22, 0.000001);
-		
+		assertEquals(9.0f*scale.z, m3f.m22, 0.000001);	
+	}
+	
+	@Test
+	public void testEquals(){
+		Matrix3f tm = new Matrix3f(1,2,3,4,5,6,7,8,9);
+		assertTrue( m3f.equals(tm) );
+		assertTrue( m3f.equals(m3f) );
+		assertFalse( m3f.equals(25) );
+		tm = new Matrix3f(2,2,3,4,5,6,7,8,9);
+		assertFalse( m3f.equals(tm) );
+		tm = new Matrix3f(1,2.1f,3,4,5,6,7,8,9);
+		assertFalse( m3f.equals(tm) );
+		tm = new Matrix3f(1,2,2.99f,4,5,6,7,8,9);
+		assertFalse( m3f.equals(tm) );
+		tm = new Matrix3f(1,2,3,4.5f,5,6,7,8,9);
+		assertFalse( m3f.equals(tm) );
+		tm = new Matrix3f(1,2,3,4,500,6,7,8,9);
+		assertFalse( m3f.equals(tm) );
+		tm = new Matrix3f(1,2,3,4,5,60,7,8,9);
+		assertFalse( m3f.equals(tm) );
+		tm = new Matrix3f(1,2,3,4,5,6,7.000001f,8,9);
+		assertFalse( m3f.equals(tm) );
+		tm = new Matrix3f(1,2,3,4,5,6,7,8.1f,9);
+		assertFalse( m3f.equals(tm) );
+		tm = new Matrix3f(1,2,3,4,5,6,7,8,9.1f);
+		assertFalse( m3f.equals(tm) );
 	}
 }
--- trunk/math/Matrix4fTest.java
+++ trunk/math/Matrix4fTest.java
@@ -3,78 +3,83 @@ package com.jme3.math;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.Before;
 
 public class Matrix4fTest 
 {
+	private Matrix4f m1;
+	private Matrix4f m2;
+	
+	@Before
+	public void setUp(){
+		m1 = new Matrix4f(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
+		m2 = new Matrix4f();
+	}
 
 	@Test
 	public void testCalculateLeibnizIdentity()
 	{
-		Matrix4f matrix = new Matrix4f();
-		matrix.calculateLeibniz();
+		m2.calculateLeibniz();
 		
-		assertEquals(matrix.fA0, 1.0f, 0.000001);
-		assertEquals(matrix.fA1, 0.0f, 0.000001);
-		assertEquals(matrix.fA2, 0.0f, 0.000001);
-		assertEquals(matrix.fA3, 0.0f, 0.000001);
-		assertEquals(matrix.fA4, 0.0f, 0.000001);
-		assertEquals(matrix.fA5, 0.0f, 0.000001);
+		assertEquals(m2.fA0, 1.0f, 0.000001);
+		assertEquals(m2.fA1, 0.0f, 0.000001);
+		assertEquals(m2.fA2, 0.0f, 0.000001);
+		assertEquals(m2.fA3, 0.0f, 0.000001);
+		assertEquals(m2.fA4, 0.0f, 0.000001);
+		assertEquals(m2.fA5, 0.0f, 0.000001);
 		
-		assertEquals(matrix.fB0, 0.0f, 0.000001);
-		assertEquals(matrix.fB1, 0.0f, 0.000001);
-		assertEquals(matrix.fB2, 0.0f, 0.000001);
-		assertEquals(matrix.fB3, 0.0f, 0.000001);
-		assertEquals(matrix.fB4, 0.0f, 0.000001);
-		assertEquals(matrix.fB5, 1.0f, 0.000001);
+		assertEquals(m2.fB0, 0.0f, 0.000001);
+		assertEquals(m2.fB1, 0.0f, 0.000001);
+		assertEquals(m2.fB2, 0.0f, 0.000001);
+		assertEquals(m2.fB3, 0.0f, 0.000001);
+		assertEquals(m2.fB4, 0.0f, 0.000001);
+		assertEquals(m2.fB5, 1.0f, 0.000001);
 	}
 	
 	@Test
 	public void testCalculateLeibniz()
 	{
-		Matrix4f matrix = new Matrix4f(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
-		matrix.calculateLeibniz();
+		m1.calculateLeibniz();
 		
-		assertEquals(matrix.fA0, -4.0f, 0.000001);
-		assertEquals(matrix.fA1, -8.0f, 0.000001);
-		assertEquals(matrix.fA2, -12.0f, 0.000001);
-		assertEquals(matrix.fA3, -4.0f, 0.000001);
-		assertEquals(matrix.fA4, -8.0f, 0.000001);
-		assertEquals(matrix.fA5, -4.0f, 0.000001);
+		assertEquals(m1.fA0, -4.0f, 0.000001);
+		assertEquals(m1.fA1, -8.0f, 0.000001);
+		assertEquals(m1.fA2, -12.0f, 0.000001);
+		assertEquals(m1.fA3, -4.0f, 0.000001);
+		assertEquals(m1.fA4, -8.0f, 0.000001);
+		assertEquals(m1.fA5, -4.0f, 0.000001);
 		
-		assertEquals(matrix.fB0, -4.0f, 0.000001);
-		assertEquals(matrix.fB1, -8.0f, 0.000001);
-		assertEquals(matrix.fB2, -12.0f, 0.000001);
-		assertEquals(matrix.fB3, -4.0f, 0.000001);
-		assertEquals(matrix.fB4, -8.0f, 0.000001);
-		assertEquals(matrix.fB5, -4.0f, 0.000001);
+		assertEquals(m1.fB0, -4.0f, 0.000001);
+		assertEquals(m1.fB1, -8.0f, 0.000001);
+		assertEquals(m1.fB2, -12.0f, 0.000001);
+		assertEquals(m1.fB3, -4.0f, 0.000001);
+		assertEquals(m1.fB4, -8.0f, 0.000001);
+		assertEquals(m1.fB5, -4.0f, 0.000001);
 	}
 	
 	@Test
 	public void testSetContents()
 	{
-		Matrix4f m1 = new Matrix4f();
-		Matrix4f m2 = new Matrix4f(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
-		m1.set(m2);
+		m2.set(m1);
 		
-		assertEquals(m1.m00, 1, 0.000001);
-		assertEquals(m1.m01, 2, 0.000001);
-		assertEquals(m1.m02, 3, 0.000001);
-		assertEquals(m1.m03, 4, 0.000001);
+		assertEquals(m2.m00, 1, 0.000001);
+		assertEquals(m2.m01, 2, 0.000001);
+		assertEquals(m2.m02, 3, 0.000001);
+		assertEquals(m2.m03, 4, 0.000001);
 		
-		assertEquals(m1.m10, 5, 0.000001);
-		assertEquals(m1.m11, 6, 0.000001);
-		assertEquals(m1.m12, 7, 0.000001);
-		assertEquals(m1.m13, 8, 0.000001);
+		assertEquals(m2.m10, 5, 0.000001);
+		assertEquals(m2.m11, 6, 0.000001);
+		assertEquals(m2.m12, 7, 0.000001);
+		assertEquals(m2.m13, 8, 0.000001);
 		
-		assertEquals(m1.m20, 9, 0.000001);
-		assertEquals(m1.m21, 10, 0.000001);
-		assertEquals(m1.m22, 11, 0.000001);
-		assertEquals(m1.m23, 12, 0.000001);
+		assertEquals(m2.m20, 9, 0.000001);
+		assertEquals(m2.m21, 10, 0.000001);
+		assertEquals(m2.m22, 11, 0.000001);
+		assertEquals(m2.m23, 12, 0.000001);
 		
-		assertEquals(m1.m30, 13, 0.000001);
-		assertEquals(m1.m31, 14, 0.000001);
-		assertEquals(m1.m32, 15, 0.000001);
-		assertEquals(m1.m33, 16, 0.000001);
+		assertEquals(m2.m30, 13, 0.000001);
+		assertEquals(m2.m31, 14, 0.000001);
+		assertEquals(m2.m32, 15, 0.000001);
+		assertEquals(m2.m33, 16, 0.000001);
 	}
 
 }
